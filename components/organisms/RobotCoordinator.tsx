6a9aa39e8c9ed03c0738daf369090aa8bcd0416f
alwkins <75457552+alwkins@users.noreply.github.com>
2022-01-27 20:32:00 -0600

    Style login bar to my liking

--- components/organisms/RobotCoordinator.tsx
+++ components/organisms/RobotCoordinator.tsx
@@ -1,20 +1,34 @@
-import React, { useState } from 'react';
-import { Robot } from '../../util/types';
-import { RobotPanel } from '../molecules/RobotPanel';
-import { RobotSelector } from '../molecules/RobotSelector';
-import styles from './RobotCoordinator.module.css';
+import React, { useState } from "react";
+import { Robot } from "../../util/types";
+import { RobotPanel } from "../molecules/RobotPanel";
+import { RobotSelector } from "../molecules/RobotSelector";
+import styles from "./RobotCoordinator.module.css";
 
 export interface RobotCoordinatorProps {
   robots: Array<Robot>;
 }
 
 export const RobotCoordinator = (props: RobotCoordinatorProps) => {
-  const [ selectedRobot, setselectedRobot ] = useState(1);
+  const [selectedRobot, setselectedRobot] = useState(1);
   const { robots } = props;
   return (
-    <div className={styles.container}>
-      <RobotSelector robots={robots} selectedIndex={selectedRobot} onSelect={setselectedRobot} />
-      <RobotPanel robot={robots[selectedRobot]}/>
+    <div className={styles.outerContainer}>
+      <div className={styles.loginBar}>
+        <span>
+          <b>USER: </b>Alison
+        </span>
+        <div>
+          <b>LOG OUT</b>
+        </div>
+      </div>
+      <div className={styles.contentContainer}>
+        <RobotSelector
+          robots={robots}
+          selectedIndex={selectedRobot}
+          onSelect={setselectedRobot}
+        />
+        <RobotPanel robot={robots[selectedRobot]} />
+      </div>
     </div>
-  )
-}
+  );
+};

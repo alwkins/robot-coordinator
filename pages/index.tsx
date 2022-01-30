2c6833f661f93bb376e9d4c3991d8516d5660f4d
alwkins <75457552+alwkins@users.noreply.github.com>
2022-01-30 08:46:29 -0600

    Add firestore, getting webpack error
    TypeError: firebase_compat_app__WEBPACK_IMPORTED_MODULE_0__.default.firestore is not a function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,39 @@
-import styles from '../styles/index.module.css';
-import { RobotCoordinator } from '../components/organisms/RobotCoordinator';
-import { ROBOTS } from '../store/dummyData';
+import { firestore } from "../firebase/clientApp";
+import { useEffect, useState } from "react";
+import { RobotCoordinator } from "../components/organisms/RobotCoordinator";
+import { ROBOTS } from "../store/dummyData";
+import { Robot } from "../util/types";
+
+
+interface HomePageProps {
+  robots: Array<Robot>;
+}
 
 function HomePage() {
-  return (
-    <RobotCoordinator robots={ROBOTS} />
-  );
+  const [robots, setRobots] = useState<Robot[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const getRobots = async () => {
+    const snapshot = await firestore.collectionGroup("robots").get();
+    const paths = snapshot.docs.map((doc) => {
+      const { name, id, activeTask, isAvailable, availableTasks } = doc.data();
+      return { params: { name, id, activeTask, isAvailable, availableTasks } };
+    });
+    console.log(paths);
+    return paths;
+  };
+
+  useEffect(() => {
+    getRobots();
+  }, []);
+
+  return <RobotCoordinator robots={ROBOTS} />;
 }
 
 export default HomePage;
+
+/* export async function getServerSideProps() {
+  const robotCollection = collection(firestore,'robots');
+  console.log(robotCollection);
+  return { props: ROBOTS }
+} */

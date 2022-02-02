9c86e7e07d3cf73246e7a8dd790cc9a670e3402a
alwkins <75457552+alwkins@users.noreply.github.com>
2022-02-01 20:33:14 -0600

    Finish mocking up task failure code
    Also documented a ton of functions

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -27,6 +27,11 @@ const firestore = getFirestore(firebaseApp);
 
 export const robotsCollection = collection(firestore, "robots");
 
+/**
+ * Convert document snapshot to Robot interface
+ * @param {QueryDocumentSnapshot<DocumentData>} docSnap - Document snapshot from firebase
+ * @return {Robot} Robot contained inside docSnap
+ */
 export const docSnapToRobot = (
   // Convert document snapshot to Robot interface
   docSnap: QueryDocumentSnapshot<DocumentData>
@@ -51,7 +56,11 @@ const docSnapToTask = (docSnap: QueryDocumentSnapshot<DocumentData>): Task => {
   return { id: docSnap.id, description, activeDescription, durationS };
 };
 
-export async function getRobots() {
+/**
+ * Get all robots in 'robots' collection
+ * @return {Promise<Robot[]>} Promise
+ */
+export async function getRobots(): Promise<Robot[]> {
   // Get robot data from 'robots' collection
   try {
     const querySnap = await getDocs(robotsCollection);
@@ -62,7 +71,12 @@ export async function getRobots() {
   }
 }
 
-export async function getRobotById(id: string) {
+/**
+ * Get robot from database by object ID
+ * @param {string} id - The object ID of robot
+ * @return {Promise<Robot>} Promise
+ */
+export async function getRobotById(id: string): Promise<Robot> {
   // Get robot document from 'robots' collection by object ID
   const docRef = doc(robotsCollection, id);
   const docSnap = await getDoc(docRef);
@@ -78,13 +92,13 @@ export async function getRobotById(id: string) {
  * @param {string} robotId - The object ID of robot to start task on
  * @param {string} taskId - The task ID of task to start
  * @param {string} operator - The operator's name
- * @return {Promise<void>}
+ * @return {Promise<void>} Promise
  */
 export const startRobotTaskwCheck = async (
   robotId: string,
   taskId: string,
   operator: string
-) => {
+): Promise<void> => {
   const docRef = doc(robotsCollection, robotId);
   const newTaskStatus = await runTransaction(firestore, async (transact) => {
     const robotDoc = await transact.get(docRef);
@@ -109,9 +123,9 @@ export const startRobotTaskwCheck = async (
 /**
  * Stop robot task on specified robot
  * @param {string} robotId - The object ID of the robot
- * @return {Promise<void>}
+ * @return {Promise<void>} Promise
  */
-export const stopRobotTask = async (robotId: string) => {
+export const stopRobotTask = async (robotId: string): Promise<void> => {
   const docRef = doc(robotsCollection, robotId);
   const response = await setDoc(
     docRef,
@@ -121,7 +135,16 @@ export const stopRobotTask = async (robotId: string) => {
   return response;
 };
 
-export interface User  {
+export const failRobotTask = async (robotId: string, taskId: string): Promise<void> => {
+  // TODO Complete this write to database
+  // 1. Increment 'robot/failed tasks', 'robot/total tasks'
+  // 2. Write 'robot/last failed task'
+  // 3. Robot Coordinator would receive updates via realtime database
+  // 4. Show error pop-up with info from 'robot/last failed task'
+  return stopRobotTask(robotId);
+}
+
+interface User  {
   user: string;
   password: string;
 }
@@ -145,7 +168,7 @@ const validUsers: Array<User> = [
  * Check if username and password are found in hard-coded array of credentials
  * @param {string} username - Username
  * @param {string} password - Password
- * @return {boolean}
+ * @return {boolean} Pass or fail authentication
  */
 export const authenticateUser = (username: string, password: string): boolean => {
   // Return if user is authentic or not
--- util/RobotSimulation.ts
+++ util/RobotSimulation.ts
@@ -1,4 +1,8 @@
-import { startRobotTaskwCheck, stopRobotTask } from "../firebase/clientApp";
+import {
+  failRobotTask,
+  startRobotTaskwCheck,
+  stopRobotTask,
+} from "../firebase/clientApp";
 
 export class RobotSimulation {
   id: string;
@@ -7,32 +11,62 @@ export class RobotSimulation {
     this.id = id;
   }
 
-  startTask(taskId: string, user: string) {
-    return startRobotTaskwCheck(this.id, taskId, user)
+  /**
+   * Wrapper for write to database to indicate robot start task
+   * This might be out of order for how real world would operate
+   * Depending on application, may want simulation robot to notify the database itself
+   * @param {string} taskId - Task to start
+   * @param {string} user - Operator starting task
+   * @return {Promise<void} Promise
+   */
+  startTask(taskId: string, user: string): Promise<void> {
+    return startRobotTaskwCheck(this.id, taskId, user);
   }
 
-  startTaskExecution(taskId: string, durationS: number) {
+  /**
+   * Simulate execution of task and robot reporting back to database when finished
+   * @param {string} taskId - Task to start
+   * @param {string} durationS - Task duration in seconds
+   * @return {void}
+   */
+  startTaskExecution(taskId: string, durationS: number): void {
     // In real world, robot would report back to database when task finished
     // Simulate here with delayed function call
     setTimeout(() => {
-      stopRobotTask(this.id); // Tell database task is finished
+      this.finishTask(taskId); // Tell database task finished
       console.log(
         `Simulating robot ${this.id} reporting task ${taskId} finished`
       );
     }, durationS * 1000);
   }
 
-  stopTask() {
-    return stopRobotTask(this.id)
+  /**
+   * Wrapper for write to database to indicate robot stop task
+   * @return {Promise<void} Promise
+   */
+  stopTask(): Promise<void> {
+    return stopRobotTask(this.id);
   }
 
-  doesTaskFail() {
+  /**
+   * Randomized simulation for task succeeding or failing
+   * @return {boolean} Does task succeed or fail
+   */
+  doesTaskFail(): boolean {
     // Simulate failure 50% of the time
-    return Math.random() > 0.5
+    return Math.random() > 0.5;
   }
 
-  // finishTask(taskId: string) {
-  //   this.doesTaskFail() ? failRobotTask(this.id, taskId) : stopRobotTask(this.id)
-  // }
-
+  /**
+   * Randomized simulation for task succeeding or failing
+   * @param {string} taskId - Object ID of task to finish
+   * @return {void}
+   */
+  finishTask(taskId: string): void {
+    // This would sim failures, but failRobotTask currently not finished
+    // See clientApp.ts
+    this.doesTaskFail()
+      ? failRobotTask(this.id, taskId)
+      : stopRobotTask(this.id);
+  }
 }
